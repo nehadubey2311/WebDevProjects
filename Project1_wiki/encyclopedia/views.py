58718bab04b72f1bfa5166593e8562b939916fc5
Neha Dubey <nehadubey2311@gmail.com>
2021-02-15 13:48:43 -0500

    Project1 WIP

--- Project1_wiki/encyclopedia/views.py
+++ Project1_wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown2
 
 from . import util
 
@@ -6,5 +8,10 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def wikiPage(request, pageTitle):
+    return render(request, "encyclopedia/wikiPage.html", {
+        "content": markdown2.markdown(util.get_entry(pageTitle))
     })
 
