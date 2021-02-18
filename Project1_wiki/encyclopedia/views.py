473b53db43212f9350929333df170e03d758956b
Neha Dubey <nehadubey2311@gmail.com>
2021-02-17 23:26:02 -0500

    editing page edit, add comments

--- Project1_wiki/encyclopedia/views.py
+++ Project1_wiki/encyclopedia/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+import random
+import re
+
+import markdown2
 from django import forms
 from django.db import models
-import re
-import markdown2
-import random
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
 
 class AddPageForm(forms.Form):
-    formData = forms.CharField(label="Page Title")
-    content = forms.CharField(label="Content", widget=forms.Textarea(attrs={'rows':5,'cols':20}))
+    formData = forms.CharField(
+        label="Page Title",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control-file form-control-sm'}))
+    content = forms.CharField(
+        label="Content",
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'cols': 20,
+                   'class': 'form-control-file form-control-sm-6'}))
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -20,21 +29,26 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def searchString(request):
     if request.method == "POST":
         # accept user input string and find entry if exists
         searchString = request.POST.get('q')
         if util.get_entry(searchString):
-            return HttpResponseRedirect(reverse('encyclopedia:wikipages', \
-                kwargs={'pageTitle':searchString}))
+            return HttpResponseRedirect(reverse(
+                'encyclopedia:wikipages',
+                kwargs={'pageTitle': searchString}))
         else:
-            # find entries that match with keyword entered by making case insensitive search
+            # find entries that match with keyword entered
+            # case insensitive search
             entries = util.list_entries()
-            patternMatch = [entry for entry in entries if re.search(searchString, entry, re.IGNORECASE)]
+            patternMatch = [entry for entry in entries
+                            if re.search(searchString, entry, re.IGNORECASE)]
             return render(request, "encyclopedia/index.html", {
                 "header": "Search Results",
                 "entries": patternMatch
             })
+
 
 def wikiPage(request, pageTitle):
     try:
@@ -42,12 +56,15 @@ def wikiPage(request, pageTitle):
             "content": markdown2.markdown(util.get_entry(pageTitle)),
             "title": pageTitle
         })
-    except:
+    # If entry was not found return an error
+    except TypeError:
         return render(request, "encyclopedia/errorPage.html", {
             "errorMsg": "The requested page was not found !!"
         })
 
+
 def addPage(request):
+    # request method would be POST when form is submitted
     if request.method == "POST":
         # Accept user submitted date
         form = AddPageForm(request.POST)
@@ -58,27 +75,31 @@ def addPage(request):
             # Save new page if entry does not already exist
             if not util.get_entry(title):
                 util.save_entry(title, content)
-                return HttpResponseRedirect(reverse('encyclopedia:wikipages', kwargs={'pageTitle':title}))
+                return HttpResponseRedirect(reverse(
+                    'encyclopedia:wikipages', kwargs={'pageTitle': title}))
             else:
                 return render(request, "encyclopedia/errorPage.html", {
                     "errorMsg": "The entry already exists"
                 })
         else:
-            return HttpResponse("Form had invalid data...try again...") 
-
+            return HttpResponse("Form had invalid data...try again...")
+    # request method would be GET when `addpage` is first called
     if request.method == "GET":
         return render(request, "encyclopedia/addPage.html", {
             "form": AddPageForm()
         })
 
+
 def editPage(request, title):
     if request.method == "GET":
+        # reusing addPage form but only displaying content
+        # so as to not let user edit title of page
         return render(request, "encyclopedia/addPage.html", {
                 "form": AddPageForm({
-                    'formData':'sample title', 
                     'content': util.get_entry(title)
                 })
             })
+
 
 def randomPage(request):
     entryList = util.list_entries()
@@ -86,5 +107,4 @@ def randomPage(request):
     return render(request, "encyclopedia/wikiPage.html", {
         "content": markdown2.markdown(util.get_entry(randomSelection)),
         "title": randomSelection
-    }) 
-    
+    })
